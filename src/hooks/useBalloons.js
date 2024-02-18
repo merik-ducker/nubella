ed6b004c675c7d1c5808c587ef6aa173a970a5d3
merik.ducker <merik.ducker@gmail.com>
2024-02-17 19:28:23 -0500

    made balloons have a radius in proportion to screen size

--- src/hooks/useBalloons.js
+++ src/hooks/useBalloons.js
@@ -30,24 +30,26 @@ const useBalloons = (engine) => {
       };
   }, [engine]);
   
+  const shootBalloon = () => {
+    // Calculate the radius based on the screen size
+    const radiusPercentage = 1.5;
+    const radius = Math.max(window.innerWidth, window.innerHeight) * (radiusPercentage / 100);
 
-    const shootBalloon = () => {
-        // Balloon creation is done here
-        const radius = 30;
-        const positionX = window.innerWidth / 2;
-        const positionY = window.innerHeight - 50;
-        const balloon = Matter.Bodies.circle(positionX, positionY, radius, {
-            restitution: 0.8,
-            render: {
-                fillStyle: `hsl(${Math.random() * 360}, 100%, 75%)`,
-            },
-        });
+    const positionX = window.innerWidth / 2;
+    const positionY = window.innerHeight - 50;
+    const balloon = Matter.Bodies.circle(positionX, positionY, radius, {
+        restitution: 0.8,
+        render: {
+            fillStyle: `hsl(${Math.random() * 360}, 100%, 75%)`,
+        },
+    });
 
-        Matter.Body.setVelocity(balloon, { x: (Math.random() - 0.5) * 10, y: -Math.random() * 30 - 5 });
-        Matter.World.add(engine.world, balloon);
+    Matter.Body.setVelocity(balloon, { x: (Math.random() - 0.5) * 10, y: -Math.random() * 30 - 5 });
+    Matter.World.add(engine.world, balloon);
 
-        setBalloons((balloons) => [...balloons, balloon]);
-    };
+    setBalloons((balloons) => [...balloons, balloon]);
+};
+
 
     return { balloons, shootBalloon };
 };
